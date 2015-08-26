2e8a7878e279ba83a29c06f6d4096e2e34c446f2
Siden6 <krzysztof.suchomski@hotmail.com>
2015-08-26 17:06:36 +0200

    Default

---
+++ EFSmallSteps/EFSmallSteps/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EFSmallSteps.Startup))]
+namespace EFSmallSteps
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
